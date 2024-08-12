3ad0e8abfcf8af509d6b42a7992dea1d84dd814d
Luiz henrique <94985541+LuizHenr1que@users.noreply.github.com>
2024-08-12 18:56:07 -0300

    att

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import IconFlutuante from "./Components/IconFlutuante/IconFlutuante";
 import SidebarMobile from "./Components/Sidebar/SidebarMobile/SidebarMobile";
 import Home from "./pages/Home/Home";
 import ProjectDetails from "./pages/Projetos/ProjetoDetails/ProjetoDetails";
-import { Routes, Route } from "react-router-dom";
 import { Sobre } from "./pages/Sobre/Sobre";
 import { Formacao } from "./pages/Formacao/Formacao";
 import PageEmail from "./pages/PageEmail/PageEmail";
@@ -11,16 +11,17 @@ import PageEmail from "./pages/PageEmail/PageEmail";
 function App() {
   return (
     <div className="App">
-      <IconFlutuante />
-      <SidebarMobile />
-      <Routes>
-        <Route path="/portfolio-v3" element={<Home />} />
-        <Route path="/portfolio-v3/:id" element={<ProjectDetails />} />
-        <Route path="/sobre" element={<Sobre />} />
-        <Route path="/formacao" element={<Formacao />} />
-        <Route path="/email" element={<PageEmail />} />
-      </Routes>
-
+      <Router basename="/portfolio-v3">
+        <IconFlutuante />
+        <SidebarMobile />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:id" element={<ProjectDetails />} />
+          <Route path="/sobre" element={<Sobre />} />
+          <Route path="/formacao" element={<Formacao />} />
+          <Route path="/email" element={<PageEmail />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
